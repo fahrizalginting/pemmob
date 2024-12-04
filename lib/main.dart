741a4b39928e4372d96f7d615c09fea40d188aac
fahrizalginting <gintingfahrizal61@gmail.com>
2024-12-04 07:01:33 +0700

    pemmob

---
+++ lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'login.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Splash(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class Splash extends StatefulWidget {
+  const Splash({super.key});
+
+  @override
+  _TugasTambahan createState() => _TugasTambahan();
+}
+
+class _TugasTambahan extends State<Splash> {
+  @override
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        color: Colors.green,
+        child: const Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Fahrizal Ginting', style: TextStyle(fontSize: 19, fontWeight: FontWeight.bold),),
+            Text('221402016', style: TextStyle(fontSize: 19, fontWeight: FontWeight.bold),)
+          ],
+        ),
+      ),
+    );
+  }
+}
+g
